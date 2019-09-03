476dcd68788287d8dfd090ae92083768d7e6ec6f
Sarah H <sarahrose.henderson@gmail.com>
2019-09-03 16:54:35 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,30 +1,35 @@
-def roll_call_dwarves(dwarfs)# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   #dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
-  # dwarfs.each_with_index do | name , index |
-  #   puts "#{index + 1} #{name}"
+dwarves.each_with_index do |dwarf, index|
+  puts "#{inded + 1}. #{dwarf}"
+
+  end
+end
+roll_call_dwarves(dwarves)
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet(planeteer_calls)# code an argument here
+  # Your code here
+planeteer_calls.map {|planeteer_calls| planeteer_calls.capitalize +'1'}
+end
+
+summon_captain_planet(planeteer_calls)
+
+assorted_words = ["two", "go", "industrious", "bop"]
+def long_planeteer_calls(planeteer_calls)
+planeteer_calls.any? {|call| call.length > 4}
+
+end
+
+long_planeteer_calls(planeteer_calls)
+
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w[gouda cheddar camembert]
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-# planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-
-end
-
-# oppressed_workers.each do |oppressed_worker|
-#    puts "#{oppressed_worker.capitalize} wants to start a union!"
-# end
-
-end
-
-
-return summon_captain_planet
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+find_the_cheese(potentially_cheesy_items)
